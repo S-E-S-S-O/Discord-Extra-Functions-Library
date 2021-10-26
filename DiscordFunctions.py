1d57941711ef33bdbf865970234160ef837a5e34
LeonDev <77613154+LeonDevITA@users.noreply.github.com>
2021-10-26 22:13:59 +0200

    Update DiscordFunctions.py

--- DiscordFunctions.py
+++ DiscordFunctions.py
@@ -1,6 +1,11 @@
 import discord
 import requests
 
-class Call():
-  def start_ringing(ID):
-  
+class Guild():
+  def delete_all_channels():
+    for DelAll in ctx.guild.channels:
+      await DelAll.delete()
+
+  def create_channels(Number, *ChannelName):
+    for CreateChannels in range(Number):
+      await CreateChannels.guild.create_text_channel(ChannelName)
